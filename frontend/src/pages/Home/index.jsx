5d266f74da738f80cdbaac8f2347916623e53cac
Celso Milleo <celsomilleo@hotmail.com>
2022-08-16 00:47:02 -0300

    Feat: home page and header

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { toast } from "react-toastify";
+
+import { Container } from "./styles";
+
+export const Home = () => {
+  const history = useHistory();
+  if (!localStorage.getItem("@MedCloud:token")) {
+    history.push("/");
+    toast.error("Você deve estar logado para acessar essa pagina");
+  }
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
